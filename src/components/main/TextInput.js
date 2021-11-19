da6e261ac76d45133a17de66babb3b7c6bc137b5
firatonderce <firatonderce@gmail.com>
2021-11-19 19:01:13 +0300

    ADD basic TextInput component

---
+++ src/components/main/TextInput.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {TextInput} from 'react-native';
+
+const TextInputComponent = ({style}) => {
+  return <TextInput style={style} />;
+};
+
+export default TextInputComponent;
