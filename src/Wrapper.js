c28e77f19a10c23c24ed7b02adad165fec267ae6
Fırat Önder <firatonderce@gmail.com>
2021-12-01 09:52:34 +0300

    ADD Splash mechanism & IOS files

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import RootStack from './navigation/RootStack';
+import SplashManager from './services/SplashManager';
+
+SplashManager.displaySplash(1250);
 
 const App = () => {
   return <RootStack />;
