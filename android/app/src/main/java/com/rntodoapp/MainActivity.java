f509453bd64a51adb0feb5a8ee9bf473e0830a5b
Fırat Önder <firatonderce@gmail.com>
2021-12-02 02:19:40 +0300

    FIX Android files

--- android/app/src/main/java/com/rntodoapp/MainActivity.java
+++ android/app/src/main/java/com/rntodoapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rntodoapp;
 
+import android.os.Bundle; 
 import com.facebook.react.ReactActivity;
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +10,9 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
-  protected String getMainComponentName() {
-    return "RNToDoApp";
+ @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState); 
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); 
   }
 }
