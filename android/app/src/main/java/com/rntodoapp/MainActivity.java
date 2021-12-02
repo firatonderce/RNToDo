1082df5a668bb13eca161400bb08552c8e1673c7
Fırat Önder <firatonderce@gmail.com>
2021-12-03 02:09:36 +0300

    ADD Android splash

--- android/app/src/main/java/com/rntodoapp/MainActivity.java
+++ android/app/src/main/java/com/rntodoapp/MainActivity.java
@@ -1,7 +1,9 @@
+
+  
 package com.rntodoapp;
 
-import android.os.Bundle; 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
@@ -10,9 +12,20 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
- @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState); 
-    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this); 
+  @Override
+  protected String getMainComponentName() {
+    return "RNToDoApp";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+
+      @Override
+      protected void loadApp(String appKey) {
+        RNBootSplash.init(MainActivity.this); // <- initialize the splash screen
+        super.loadApp(appKey);
+      }
+    };
   }
 }
--- src/services/SplashManager.js
+++ src/services/SplashManager.js
@@ -1,7 +1,6 @@
 import RNBootSplash from 'react-native-bootsplash';
 
 const displaySplash = (duration = 1000) => {
-  RNBootSplash.show();
   setTimeout(() => RNBootSplash.hide({fade: true}), duration);
 };
 
